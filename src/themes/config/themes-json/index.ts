8dda7b9bd5072e33b39f1790e4e6df56556dd205
Devansh Jani <79559686+djman323@users.noreply.github.com>
2025-08-20 17:43:23 +0530

    Added multiple theme colors background

--- src/themes/config/themes-json/index.ts
+++ src/themes/config/themes-json/index.ts
@@ -4,6 +4,20 @@ import elementary from "./elementary.json";
 import sovereign from "./sovereign.json";
 import cinder from "./cinder.json";
 import artisan from "./artisan.json";
+import cyberpunk from "./cyberpunk.json";
+import dark_elegance from "./dark-elegance.json";
+import dessert_sand from "./desert-sand.json";
+import cotton_candy from "./cotton-candy.json";
+import minimal_dark from "./minimal-dark.json";
+import minimal_green from "./minimal-green.json";
+import minty_breeze from "./minty-breeze.json";
+import moss_green from "./moss-green.json";
+import ocean_mist from "./ocean-mist.json";
+import pastal_breeze from "./pastal-breeze.json";
+import retro_neon from "./retro-neon.json";;
+import sage_calm from "./sage-calm.json";
+import solarized_dark from "./solarized-dark.json";
+import sunset_glow from "./sunset-glow.json"
 import { Theme } from "../ThemeStore";
 
 export const defaultThemes: Record<string, Theme> = {
@@ -13,4 +27,19 @@ export const defaultThemes: Record<string, Theme> = {
   [sovereign.name]: sovereign,
   [cinder.name]: cinder,
   [artisan.name]: artisan,
+  [cyberpunk.name]: cyberpunk,
+  [dark_elegance.name]: dark_elegance,
+  [dessert_sand.name]: dessert_sand,
+  [cotton_candy.name]: cotton_candy,
+  [minimal_dark.name]: minimal_dark,
+  [minimal_green.name]: minimal_green,
+  [minty_breeze.name]: minty_breeze,
+  [moss_green.name]: moss_green,
+  [ocean_mist.name]: ocean_mist,
+  [pastal_breeze.name]: pastal_breeze,
+  [retro_neon.name]: retro_neon,
+  [sage_calm.name]: sage_calm,
+  [solarized_dark.name]: solarized_dark,
+  [sunset_glow.name]: sunset_glow,
+
 };
